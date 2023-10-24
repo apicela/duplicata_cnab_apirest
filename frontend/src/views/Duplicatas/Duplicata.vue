609fd042adbf8e6e9c6ddbde8eaf682d158b8b0b
jamilzin1 <trab.jamilsouza@gmail.com>
2023-10-24 13:52:06 -0300

    table

--- frontend/src/views/Duplicatas/Duplicata.vue
+++ frontend/src/views/Duplicatas/Duplicata.vue
@@ -1,10 +1,58 @@
 <template>
-    <div class="about">
-      <h1>This is an DSADS page</h1>
+  <div>
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          duplicata
+          <RouterLink to="/duplicatas/create" class="btn btn-primary float-end">
+            Criar duplicata
+          </RouterLink>
+        </h4>
+      </div>
+      <div class="card-body">
+<table class="table table-bordered">
+<thead>
+          <tr>
+            <th>ID</th>
+            <th>Razão Social</th>
+            <th>ID Empresa</th>
+            <th>Transferências</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(duplicata, index) in this.duplicatas" :key "index">
+            <td>{{ duplicata.duplicata_id }}</td>
+
+          </tr>
+        </tbody>
+      </table>
+      </div>
     </div>
-  </template>
-  
-  <style>
-  
-  </style>
-  
+  </div>
+</template>
+<script lang="ts">
+import axios from 'axios';
+
+
+export default {
+  name: 'duplicatas',
+  data(){
+    return {
+      duplicatas:[]
+    }
+  },
+  mounted(){
+    console.log('mounted')
+    this.getAllDuplicatas();
+  },
+  methods: {
+    getAllDuplicatas(){
+      axios.get('http://localhost:8080/duplicatas').then( res => {
+        this.duplicatas = res.data
+      }).catch(err => {
+        console.error('Error fetching duplicatas', err);
+      });
+    }  
+  },
+}
+</script>
