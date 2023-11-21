b7cd2ce45552587d4bddf5e921db5bf4a181fe34
jamilzin1 <jamilnetobr@gmail.com>
2023-11-21 15:33:36 -0300

    backend test files update

--- backend/src/main/java/apicela/duplicata/shared/DuplicataGenerator.java
+++ backend/src/main/java/apicela/duplicata/shared/DuplicataGenerator.java
@@ -7,7 +7,7 @@ public class DuplicataGenerator {
 
     public List<String> criarDuplicataValida() {
         List<String> duplicata = new ArrayList<>();
-        duplicata.add(CommonStrings.CACABECALHO_HOME_DEVELOPER);
+        duplicata.add(CommonStrings.CACABECALHO_TEST_DEVELOPER);
         duplicata.add(CommonStrings.TRANSACAO_OK_01);
         duplicata.add(CommonStrings.TRANSACAO_OK_02);
         duplicata.add(CommonStrings.TRANSACAO_OK_03);
